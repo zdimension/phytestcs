ad988cbc2b590d60310706f2e6db331e2cea3a64
zdimension <zippedfire@free.fr>
2020-07-23 00:42:55 +0200

    add special properties window

---
+++ phytestcs/Interface/Windows/Properties/WndSpecial.cs
@@ -0,0 +1,17 @@
+﻿using phytestcs.Objects;
+using SFML.System;
+
+namespace phytestcs.Interface.Windows.Properties
+{
+    public class WndSpecial : WndBase<PhysicalObject>
+    {
+        public WndSpecial(PhysicalObject obj, Vector2f pos)
+            : base(obj, 250, pos)
+        {
+            Add(new NumberField<float>(0.01f, 100, bindProp: () => obj.AirFrictionMultiplier, log: true) { LeftValue = 0 });
+            Add(new NumberField<float>(0.01f, 100, bindProp: () => obj.InertiaMultiplier, log: true) { LeftValue = 0 });
+
+            Show();
+        }
+    }
+}

6983236d4be7bbf8f2d2e277a81406ff8dfb349b
zdimension <zippedfire@free.fr>
2020-07-24 04:53:37 +0200

    add Repr for enumerables, colors and vectors

--- phytestcs/ToStringProxy.cs
+++ phytestcs/ToStringProxy.cs
@@ -30,8 +30,70 @@ namespace phytestcs
             if (tuple == null) throw new ArgumentNullException(nameof(tuple));
             return $"({string.Join(", ", Enumerable.Range(0, tuple.Length).Select(x => Repr(tuple[x])))})";
         }
-        
-        public static string Repr(this object? o, int maxDepth=5, int currentDepth=0)
+
+        public static string Repr(this IEnumerable<object> coll, int maxDepth=5, int currentDepth=0)
+        {
+            var res = new StringBuilder();
+            res.Append("[");
+            if (currentDepth > maxDepth)
+            {
+                res.Append(" ... ]");
+            }
+            else
+            {
+                res.AppendJoin(',', 
+                    coll
+                        .Select(prop =>
+                        {
+                            var r = "\n\t";
+
+                            try
+                            {
+                                r += Repr(prop, maxDepth, currentDepth + 1).IndentBlock();
+                            }
+                            catch (Exception e)
+                            {
+                                r += L["<error>"];
+                            }
+
+                            return r;
+                        }));
+                res.Append("\n]");
+            }
+
+            return res.ToString();
+        }
+
+        public static string IndentBlock(this string s)
+        {
+            var lines = s.Split('\n');
+            var r = new StringBuilder(lines[0]);
+            for (var i = 1; i < lines.Length; i++)
+                r.Append("\n\t" + lines[i]);
+            return r.ToString();
+        }
+
+        public static string Repr(this Color o)
+        {
+            return (o.R, o.G, o.B, o.A).Repr();
+        }
+
+        public static string Repr(this Vector2f o)
+        {
+            return (o.X, o.Y).Repr();
+        }
+
+        public static string Repr(this bool b)
+        {
+            return b ? "true" : "false";
+        }
+
+        public static string Repr(this string s)
+        {
+            return $"\"{s}\"";
+        }
+
+        public static string Repr(this object? o, int maxDepth=5, int currentDepth=0, bool hideFirstType=false)
         {
             if (o == null)
                 return "null";
